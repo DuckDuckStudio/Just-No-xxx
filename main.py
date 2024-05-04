ffc6f5e2109cc1a89f740a5d12ce89981e63fdb4
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-05-05 02:24:12 +0800

    上传本地文件

---
+++ main.py
@@ -0,0 +1,52 @@
+import psutil
+import os
+import time
+from tkinter import messagebox
+from configparser import ConfigParser
+
+# -------------相关设置-----------------
+nc_processes = ".\\Processes.txt" # 需要检查的进程(Need Check Processes)
+# 每次检查间隔时间请在config.ini中修改，时间过短可能会影响CPU
+def punish():
+    # 可以在这里编辑需要罚什么
+    # 例如 os.system("shutdown -s -t 30") # 30秒后关机
+    # 当然，你也可以将 30 秒改为 0 秒(立刻)，真就成防沉迷了属于是
+    os.system("shutdown -s -t 30")
+    pass
+# -------------------------------------
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))  # 避免意外的位置
+config = ConfigParser(comment_prefixes=[])
+config.read("config.ini", encoding='utf-8')
+wait_time = config.getfloat('settings', 'wait_time')
+
+# 读取需要检查的进程名称列表
+def read_processes(file_path):
+    with open(file_path, 'r') as file:
+        processes = file.readlines()
+    # 移除每行末尾的换行符
+    processes = [process.strip() for process in processes]
+    return processes
+
+# 检查进程是否在运行
+def is_process_running(process_name):
+    for process in psutil.process_iter(['name']):
+        if process.info['name'] == process_name:
+            return True
+    return False
+
+def should_punish(process_name):# 该罚
+    messagebox.showwarning("你在干什么？！", f"你竟然打开了{process_name}？！\n你怎么敢的？")
+    punish()
+
+def main():
+    processes_to_check = read_processes(nc_processes)
+    print("读取配置文件完毕！")
+    while True:
+        for process_name in processes_to_check:
+            if is_process_running(process_name):
+                should_punish(process_name)
+        time.sleep(wait_time)
+
+if __name__ == "__main__":
+    main()
---
+++ no-console/v1.0/Set-Start.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))  # 避免意外的位置
+
+# -------------相关设置-----------------
+# 最好别乱动 :D
+startup_folder = os.path.join(os.getenv('APPDATA'), "Microsoft\\Windows\\Start Menu\\Programs\\Startup")
+shortcut_name = "Check-Processes.lnk"# 在启动文件夹中的快捷方式名，如需修改请先清除自启动
+shortcut_path = os.path.join(startup_folder, shortcut_name)
+source_file = ".\\main.exe"# 主程序路径
+# -------------------------------------
+
+if os.path.exists(shortcut_path):
+    # 存在
+    print("目前已被设为自启动，是否取消设置？")
+    while True :
+        print("[Y]是 [N]否")
+        temp = input("你的回答是：")
+        if temp.lower() in ["y", "yes", "是"]:
+            print("删除自启动项中...")
+            os.remove(shortcut_path)
+            print("删除自启动项完毕！")
+            break
+        elif temp.lower() in ["n", "no", "否", "不"]:
+            print("取消操作...")
+            break
+            # 退出
+        else:
+            print("请按说明回答！")
+else:
+    # 不存在
+    print("目前没有被设为自启动，是否设置自启动？")
+    while True :
+        print("[Y]是 [N]否")
+        temp = input("你的回答是：")
+        if temp.lower() in ["y", "yes", "是"]:
+            print("创建自启动项中...")
+            # 在启动文件夹中创建快捷方式
+            shutil.copyfile(source_file, shortcut_path)
+            print("创建自启动项完毕！")
+            break
+        elif temp.lower() in ["n", "no", "否", "不"]:
+            print("取消操作...")
+            break
+            # 退出
+        else:
+            print("请按说明回答！")
+
+input("按Enter键继续...")
